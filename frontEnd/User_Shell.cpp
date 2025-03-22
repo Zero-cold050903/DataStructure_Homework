2eb10aa136e444f2b39c1fd4f09dac1148c759e4
Dylanliu <pxyforeverlove1314@gmail.com>
2025-03-22 16:03:28 +0800

    Front end page constructinggit add User_Shell.cpp

---
+++ frontEnd/User_Shell.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+void Login_Page()
+{
+	cout<<"------------------------------------\n"
+	    <<"|       TSOS  Ver 0.0.1(beta)      |\n"
+	    <<"*   Welcome to the TSOS,you are:   *\n"
+	    <<"|     CHOOSE: (1) log in as T      |\n"
+	    <<"*             (2) log in as S      *\n"
+	    <<"|    I'm ____    (choose 1/2 plz)  |\n"
+	    <<"------------------------------------\n";
+}
+void Log()
+{
+	cout<<"------------------------------------\n"
+	    <<"| |\n"
+	    <<"*                                  *\n"
+	    <<"|                                |
+	    <<"**
+	    <<"||
+	    <<"**
+	    <<"||
+	    <<"**
+	    <<"------------------------------------\n"
+
